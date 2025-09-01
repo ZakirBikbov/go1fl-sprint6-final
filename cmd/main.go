24452c9624a91ac0e821c7528d4fc946e05c06c0
Zakir Bikbov <z.bikbov@jgarant.kz>
2025-09-01 16:12:48 +0500

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stderr, "morse-app ", log.LstdFlags)
+
+	srv := server.NewServer(logger)
+
+	logger.Println("Starting server on :8080")
+	if err := srv.HTTP.ListenAndServe(); err != nil {
+		logger.Fatal(err)
+	}
 }
